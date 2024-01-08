32b7edf781e20b619b8acccf1ed241f7961db26d
Ricardo Gomes <mail@ricardogomes.me>
2024-01-08 20:55:40 +0000

    chore: move bashrc config to separate file

--- modules/applications/shells/bash/home-manager.nix
+++ modules/applications/shells/bash/home-manager.nix
@@ -1,24 +1,12 @@
 { config, pkgs, vars, ... }:
-
+let
+  bashrcConfig = builtins.readFile ./resources/bashrc;
+in
 {
     programs = { 
   		bash = {
 			enable = true;
-			bashrcExtra = ''
-
-				# Starship Prompt
-				eval "$(starship init bash)"
-
-				# Laravel SAIL Bootstrap
-				sail_bootstrap() (
-					docker run --rm \
-						-u "$(id -u):$(id -g)" \
-				    	-v "$(pwd):/var/www/html" \
-				    	-w /var/www/html \
-				   		laravelsail/php82-composer:latest \
-					    composer install --ignore-platform-reqs
-				)
-			'';
+			bashrcExtra = bashrcConfig
 	 	};
 	};
 }
