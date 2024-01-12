583c1fdaf0f8539d44dee566d738a630466406d0
Ricardo Gomes <mail@ricardogomes.me>
2024-01-12 12:06:58 +0000

    feat: [nvim] improve nvim-tree behaviour
    [nvim] replace keymaps for moving between panes (removed the w)

--- modules/applications/editors/nvim/lua/config/keymaps.lua
+++ modules/applications/editors/nvim/lua/config/keymaps.lua
@@ -5,7 +5,14 @@ local default_opts = {
 	silent = true,
 }
 
+-- Navigation
 vim.keymap.set("n","<leader>bn", ":bnext<CR>", default_opts)
+vim.keymap.set("n","<C-h>","<C-w>h",default_opts)
+vim.keymap.set("n","<C-j>","<C-w>j",default_opts)
+vim.keymap.set("n","<C-k>","<C-w>k",default_opts)
+vim.keymap.set("n","<C-l>","<C-w>l",default_opts)
 
+-- Plugins
+--- Nvim-tree
 vim.keymap.set("n","<leader>m", ":NvimTreeFocus<CR>", default_opts)
 vim.keymap.set("n","<leader>e", ":NvimTreeToggle<CR>", default_opts)
--- modules/applications/editors/nvim/lua/plugins/nvim-tree.lua
+++ modules/applications/editors/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,22 @@
+-- Global Variables
+local tree_open = false
+
+-- Functions
 local function open_tab_silent(node)
     local api = require("nvim-tree.api")
     api.node.open.tab(node)
     vim.cmd.tabprev()
 end
 
+local function tab_enter()
+    local api = require('nvim-tree.api')
+    if tree_open then
+        api.tree.open()
+        vim.api.nvim_command('wincmd p')
+    else
+        api.tree.close()
+    end
+end
 
 return {
 	"nvim-tree/nvim-tree.lua",
@@ -21,6 +34,10 @@ return {
 			view = {
                 adaptive_size = true,
 			},
+            renderer = {
+                highlight_opened_files = 'icon',
+
+            },
             on_attach = function(bufnr)
                 local api = require("nvim-tree.api")
 
@@ -28,12 +45,18 @@ return {
                     return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
                 end
 
+                -- Keymaps
                 api.config.mappings.default_on_attach(bufnr)
 
                 vim.keymap.set('n','<CR>', api.node.open.tab_drop, opts('Tab Drop'))
                 vim.keymap.set('n', '<2-LeftMouse>',  api.node.open.tab_drop, opts('Open'))
                 vim.keymap.set('n','T', open_tab_silent, opts('Open Tab Silent'))
 
+                -- Events
+
+                api.events.subscribe(api.events.Event.TreeOpen, function() tree_open=true end)
+                api.events.subscribe(api.events.Event.TreeClose, function() tree_open=false end)
+                vim.api.nvim_create_autocmd({"TabEnter","TabNewEntered"}, {callback=tab_enter})
             end
 		})
 	end,
