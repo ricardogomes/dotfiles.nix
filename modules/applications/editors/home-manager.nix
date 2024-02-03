1b527ac5a25efd6f89d3c161601afd9b0a0df0dd
Ricardo Gomes <mail@ricardogomes.me>
2024-02-03 16:28:33 +0000

    feat: adding nix version of emacs config

--- modules/applications/editors/home-manager.nix
+++ modules/applications/editors/home-manager.nix
@@ -9,7 +9,14 @@
 			vimAlias = true;
 			vimdiffAlias = true;
 		};	
+        emacs = {
+            enable = true;
+        };
 	};
+
+    services = {
+        emacs = enable;
+    };
 
 	home.file = {
 		".config/nvim/init.lua".source = ./nvim/init.lua;
@@ -17,6 +24,7 @@
 			source = ./nvim/lua;
 			recursive = true;
 		};
+        ".config/emacs/init.el".source = ./emacs/init.el;
 	};
 
 }
