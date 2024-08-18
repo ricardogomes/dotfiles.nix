308fad4a4abd4432377aa0c4060b397bfed77c6a
Ricardo Gomes <ricardo@bytencode.com>
2024-08-18 14:02:46 +0100

    feat: add minecraft server to beelinknix

--- hosts/beelinknix/services.nix
+++ hosts/beelinknix/services.nix
@@ -69,6 +69,7 @@
             enable = true;
             eula = true;
             declarative = true;
+            package = pkgs.minecraft-server_1_21_1
             serverProperties = {
                 level-name = "vilage world 1.21";
                 level-seed = "2143341957457548";
