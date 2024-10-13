8fa5f92fec7b8ac73bb5249bfa77eb66bb3ef33f
Ricardo Gomes <mail@ricardogomes.me>
2024-10-13 16:08:01 +0100

    chore: update samba syntax on beelinknix

--- hosts/beelinknix/services.nix
+++ hosts/beelinknix/services.nix
@@ -4,36 +4,38 @@
     services = {
         samba = {
             enable = true;
-            securityType = "user";
             settings = {
-                workgroup = "WORKGROUP";
-                "server role" = "standalone server";
-                "dns proxy" = "no";
-                "pam password change" = "yes";
-                "map to guest" = "bad user";
-                "usershare allow guests" = "yes";
-                "create mask" = "0666";
-                "force create mode" = "0666";
-                "directory mask" = "0777";
-                "force directory mode" = "0777";
-                "follow symlinks" = "yes";
-                "load printers" = "no";
-                "printing" = "bsd";
-                "printcap name" = "/dev/null";
-                "disable spoolss" = "yes";
-                "strict locking" = "no";
-                #"aio read size" = 0;
-                #"aio write size" = 0;
-                "vfs objects" = "acl_xattr catia fruit streams_xattr";
-                "inherit permissions" = "yes";
+                global = {
+                    security = "user";
+                    workgroup = "WORKGROUP";
+                    "server role" = "standalone server";
+                    "dns proxy" = "no";
+                    "pam password change" = "yes";
+                    "map to guest" = "bad user";
+                    "usershare allow guests" = "yes";
+                    "create mask" = "0666";
+                    "force create mode" = "0666";
+                    "directory mask" = "0777";
+                    "force directory mode" = "0777";
+                    "follow symlinks" = "yes";
+                    "load printers" = "no";
+                    "printing" = "bsd";
+                    "printcap name" = "/dev/null";
+                    "disable spoolss" = "yes";
+                    "strict locking" = "no";
+                    #"aio read size" = 0;
+                    #"aio write size" = 0;
+                    "vfs objects" = "acl_xattr catia fruit streams_xattr";
+                    "inherit permissions" = "yes";
 
-                # Security
-                "client ipc max protocol" = "SMB3";
-                "client ipc min protocol" = "SMB2_10";
-                "client max protocol" = "SMB3";
-                "client min protocol" = "SMB2_10";
-                "server max protocol" = "SMB3";
-                "server min protocol" = "SMB2_10";
+                    # Security
+                    "client ipc max protocol" = "SMB3";
+                    "client ipc min protocol" = "SMB2_10";
+                    "client max protocol" = "SMB3";
+                    "client min protocol" = "SMB2_10";
+                    "server max protocol" = "SMB3";
+                    "server min protocol" = "SMB2_10";
+                } 
            
                 data = {
                     path = "/data";
