37b21a3473fcddd3ee67e6afeaf45181893922bf
Ricardo Gomes <mail@ricardogomes.me>
2024-11-17 14:46:33 +0000

    feat: working with insecure docker registries - IPLeiria - DAD

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -5,7 +5,13 @@
 	virtualisation = {
 		docker = {
 			enable = true;
-		    extraOptions = "--default-address-pool base=10.10.0.0/16,size=24";
+			daemon = {
+				settings = {
+					debug = true;
+					"log-level" = "debug";
+				};
+			};
+		    extraOptions = "--insecure-registry registry.172.22.21.107.sslip.io --registry-mirror http://registry.172.22.21.107.sslip.io --default-address-pool base=10.10.0.0/16,size=24";
 		};
 
 		libvirtd = {
