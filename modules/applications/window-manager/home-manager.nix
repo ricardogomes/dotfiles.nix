3bdb637ae7a57def53a97609b82536d181d13271
Ricardo Gomes <ricardo@bytencode.com>
2025-07-22 17:17:29 +0100

    feat: working on i3

--- modules/applications/window-manager/home-manager.nix
+++ modules/applications/window-manager/home-manager.nix
@@ -1,7 +1,15 @@
 { config, pkgs, vars, ... }:
 
 {
-    home.packages = with pkgs;[
-        
-    ];
+    xsession.windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+        config = {
+        modifier = "Mod4";
+            gaps = {
+                inner = 10;
+                outer = 5;
+            };
+        };
+    };
 }
